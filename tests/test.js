954a4e1aec4a804306d37fac18a2882fde5b947f
Dídac García <didaquis@gmail.com>
2018-03-14 11:14:07 +0100

    :pencil: Preparando los ficheros para TDD

---
+++ tests/test.js
@@ -0,0 +1,10 @@
+
+const assert = require('assert');
+
+describe('Testing server API', () => {
+
+	it('should list collections', () => {
+		assert.equal('OK', 'OK', 'result should be OK');
+	});
+
+});

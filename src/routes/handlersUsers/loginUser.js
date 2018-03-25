4498bfd47f87985168d6ed8643d9b099c09cfa8c
Dídac García <didaquis@gmail.com>
2018-03-25 17:12:01 +0200

    :pencil: La API ahora envía el id del usuario

--- src/routes/handlersUsers/loginUser.js
+++ src/routes/handlersUsers/loginUser.js
@@ -1,4 +1,3 @@
-//const logic = require('../../logic');
 const { success, fail } = require('../../utils/api-helpers');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET: secret, JWT_EXP: expiration } = process.env;
@@ -6,19 +5,16 @@ const expiresIn = parseInt(expiration);
 
 module.exports = (req, res) => {
 	const { user } = req;
-	
-	try {
-		const token = jwt.sign({
-			id: user._id,
-			username: user.username
-		}, secret, { expiresIn });
-		
-		const response = {
-			token,
-			username: user.username
-		};
-		res.json(success(response));
-	} catch (err) {
-		res.json(fail(err.message));
-	}
+
+	const token = jwt.sign({
+		id: user._id,
+		username: user.username
+	}, secret, { expiresIn });
+
+	const response = {
+		token,
+		username: user.username,
+		id: user._id
+	};
+	res.json(success(response));
 };
